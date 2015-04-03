0d09684b06161754cf677456fef655a2c49a2523
Din <dy13145687@gmail.com>
2015-04-03 15:15:56 +0800

    [info #1]init

---
+++ leo-parent/leo-core/src/test/java/com/genghis/lucene/TestLucene.java
@@ -0,0 +1,24 @@
+package com.genghis.lucene;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by Din on 2015/3/30.
+ */
+public class TestLucene {
+
+
+
+
+}
